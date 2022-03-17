8bb830ec9105f577a9a140123e006280c9c9b9b5
Pam <pamellabviana@gmail.com>
2022-03-17 09:58:01 -0400

    commit

--- src/components/Cards/cards.component.jsx
+++ src/components/Cards/cards.component.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./cards.styles.css";
+import PictureIcon from '../../images/image-icon.svg';
+import CameraIcon from '../../images/camera-icon.svg';
+import TypeIcon from '../../images/type-icon.svg';
 
 function Cards(props) {
   return (
     <div className="card-section">
       <div className="upload-box card-wrapper">
+       <img alt="upload icon" src={PictureIcon}/>
         <p>Upload Your Picture</p>
       </div>
       <div className="camera-box card-wrapper">
+      <img alt="camera icon" src={CameraIcon}/>
         <p>Take a Picture</p>
       </div>
       <div className="type-box card-wrapper">
+      <img alt="type icon" src={TypeIcon}/>
         <p>Type Your Equation</p>
       </div>
     </div>
