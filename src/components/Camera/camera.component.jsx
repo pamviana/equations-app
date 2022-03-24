ad4f50b6331014eadc620410fd6407cbd440ec6a
Pam <pamellabviana@gmail.com>
2022-03-24 16:08:06 -0400

    added camera

--- src/components/Camera/camera.component.jsx
+++ src/components/Camera/camera.component.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import './camera.styles.css';
+import React, { useEffect, useRef} from "react";
+import "./camera.styles.css";
 
-function Camera(){
-    return(
-        <p> Camera</p>
-    )
+function Camera() {
+    const videoRef= useRef(null);
+
+    useEffect(() => {
+        getVideo();
+      }, [videoRef]);
+      
+    const getVideo = () => {
+        navigator.mediaDevices
+          .getUserMedia({ video: { width: 300 } })
+          .then(stream => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+          })
+          .catch(err => {
+            console.error("error:", err);
+          });
+      };
+
+  return (
+    <>
+      <button>Take a Picture</button>
+      <video ref={videoRef}/>
+      <canvas/>
+    </>
+  );
 }
 
 export default Camera;
