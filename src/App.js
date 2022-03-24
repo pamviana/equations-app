686d2698a40582256238f70ea8c1575de45a6522
Pam <pamellabviana@gmail.com>
2022-03-24 13:13:27 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import React, { useState } from "react";
 import HomePage from "./components/HomePage/home-page.component";
+import { Route, Routes, BrowserRouter as Router, Link } from "react-router-dom";
 import HamburguerMenu from "./images/hamburguer-menu-icon.svg";
+import UpdatePicture from "./components/UpdatePicture/drop-zone.component";
+import Result from "./components/Result/result.component";
+import Camera from "./components/Camera/camera.component";
+import TypeEquation from "./components/TypeEquation/type.component";
+import Cards from "./components/Cards/cards.component";
+import CloseIcon from "./images/close-x.svg";
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
@@ -9,30 +16,41 @@ function App() {
 
   return (
     <div className="App">
-      <div className="center-main-box">
-        <header className="header-main-box">
-          <img
-            id="hamburguer-menu-icon"
-            alt="menu icon"
-            src={HamburguerMenu}
-            onClick={handleClickSidebar}
-          />
-          <nav
-            className={sidebar ? "side-menu active" : "side-menu"}
-            onClick={handleClickSidebar}
-          >
-            <ul className="sidebar-container">
-              <li>Home</li>
-              <li>Upload a Picture</li>
-              <li>Take a Picture</li>
-              <li>Type an Equation</li>
-            </ul>
-          </nav>
-        </header>
-        <main className="main-box-container">
-          <HomePage />
-        </main>
-      </div>
+      <Router>
+        <div className="center-main-box">
+          <header className="header-main-box">
+            <img
+              id="hamburguer-menu-icon"
+              alt="menu icon"
+              src={HamburguerMenu}
+              onClick={handleClickSidebar}
+            />
+            <div className={sidebar ? "sidebar-filler active" : "sidebar-filler"}></div>
+            <nav
+              className={sidebar ? "side-menu active" : "side-menu"}              
+            >
+            
+              <span className="close-icon sidebar-icon" onClick={handleClickSidebar}></span>
+
+              <ul className="sidebar-container">
+                <Link to="/">Home</Link>
+                <Link to="/upload">Upload a Picture</Link>
+                <Link to="/camera">Take a Picture</Link>
+                <Link to="/type">Type an Equation</Link>
+              </ul>
+            </nav>
+          </header>
+          <main className="main-box-container">
+            <Routes>
+              <Route exact path="/" element={<Cards />} />
+              <Route path="/upload" element={<UpdatePicture />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="/camera" element={<Camera />} />
+              <Route path="/type" element={<TypeEquation />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </div>
   );
 }
