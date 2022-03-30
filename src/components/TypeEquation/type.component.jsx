6155bb749e20e0d873bd81b486e84981b9b49134
Pam <pamellabviana@gmail.com>
2022-03-29 22:18:34 -0400

    commit

--- src/components/TypeEquation/type.component.jsx
+++ src/components/TypeEquation/type.component.jsx
@@ -7,7 +7,7 @@ addStyles();
 
 function TypeEquation(props) {
   let navigate = useNavigate();
-  const [latex, setLatex] = useState("\\frac{1}{\\sqrt{2}}\\cdot 2");
+  const [latex, setLatex] = useState("");
 
 
   useEffect(()=> {
